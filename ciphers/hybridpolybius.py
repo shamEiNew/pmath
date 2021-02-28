0df4d12cdc959cfe781c429f4dc7c695c890a263
shamEiNew <panchalshamsundar7@gmail.com>
2021-02-28 23:33:33 +0530

    nice

--- ciphers/hybridpolybius.py
+++ ciphers/hybridpolybius.py
@@ -1,6 +1,6 @@
 def create_playfair_msg():
 
-    plain_text = "HELLO WORLD"
+    plain_text = "LOVE YOU"
     plain_text = plain_text.replace(" ","")
     plain_text_stripped = [plain_text[i] for i in range(0,len(plain_text))]
     playfair_message = []
@@ -45,71 +45,124 @@ def transpose(playfair_key):
         playfair_key_transpose.append(temp)
     return playfair_key_transpose
 
-    
-playfair_message = create_playfair_msg()
-print(playfair_message)
-playfair_key = create_playfair_key('PLAYFAIR')
-print(playfair_key)
-playfair_key_transpose = transpose(playfair_key)
-print(playfair_key_transpose)
 
-enc = []
+def encrypt_message():
+    enc = []
 
-for j in range(0, len(playfair_message)):
-    flag = 0
-    for i in range(0, len(playfair_key)):
+    for j in playfair_message:
 
-        if (
-            playfair_message[j][0] in playfair_key[i]
-            and 
-            playfair_message[j][1] in playfair_key[i]
-            ):
-            enc.append(
-                playfair_key[i][(playfair_key[i].index(playfair_message[j][0])+1)%5]
-                +
-                playfair_key[i][(playfair_key[i].index(playfair_message[j][1])+1)%5]
-                )
+        for i in range(0, len(playfair_key)):
+
+            if (j[0] in playfair_key[i] and j[1] in playfair_key[i]):
+                enc.append(
+                    playfair_key[i][(playfair_key[i].index(j[0])+1)%5]
+                    +
+                    playfair_key[i][(playfair_key[i].index(j[1])+1)%5]
+                    )
 
 
-        elif(
-            playfair_message[j][0] in playfair_key_transpose[i]
-            and 
-            playfair_message[j][1] in playfair_key_transpose[i]
-            ):
-            enc.append(
-                playfair_key_transpose[i][(playfair_key_transpose[i].index(playfair_message[j][0])+1)%5]
-                +
-                playfair_key_transpose[i][(playfair_key_transpose[i].index(playfair_message[j][1])+1)%5]
-                )
-        else:
-            if(
-                playfair_message[j][0] in playfair_key[i]
-                and
-                playfair_message[j][1] not in playfair_key[i]
+            elif(
+                j[0] in playfair_key_transpose[i]
+                and 
+                j[1] in playfair_key_transpose[i]
                 ):
-                for _ in range(0, len(playfair_key_transpose)):
-                    if playfair_message[j][0] in playfair_key_transpose[_] and playfair_message[j][1] not in playfair_key_transpose[_]:
-                        for k in range(0, len(playfair_key)):
-                            if(
-                                playfair_message[j][1] in playfair_key[k]
-                                ):
+                enc.append(
+                    playfair_key_transpose[i][(playfair_key_transpose[i].index(j[0])+1)%5]
+                    +
+                    playfair_key_transpose[i][(playfair_key_transpose[i].index(j[1])+1)%5]
+                    )
+            else:
+                if(
+                    j[0] in playfair_key[i]
+                    and
+                    j[1] not in playfair_key[i]
+                    ):
 
-                                row_number_1 = k
-                                column_number_1 = playfair_key[i].index(playfair_message[j][0])
+                    for _ in range(0, len(playfair_key_transpose)):
 
-                                row_number_0 = i
-                                column_number_0 = playfair_key[k].index(playfair_message[j][1])
+                        if j[0] in playfair_key_transpose[_] and j[1] not in playfair_key_transpose[_]:
 
-                                enc.append(
-                                    playfair_key[row_number_0][column_number_0]
-                                    +
-                                    playfair_key[row_number_1][column_number_1]
-                                    )
-                            flag+=1
-                            if j == 2: print(f"alla{flag}")
-    
+                            for k in range(0, len(playfair_key)):
+                                if(
+                                    j[1] in playfair_key[k]
+                                    ):
+
+                                    row_number_1 = k
+                                    column_number_1 = playfair_key[i].index(j[0])
+
+                                    row_number_0 = i
+                                    column_number_0 = playfair_key[k].index(j[1])
+
+                                    enc.append(
+                                        playfair_key[row_number_0][column_number_0]
+                                        +
+                                        playfair_key[row_number_1][column_number_1]
+                                        )
+    return enc
 
 
-                
-    #print(f"The element is {j} and flag is {flag}")    
-print(enc)
+def decrypt_message(playfair_message):
+    p = []
+
+    for j in playfair_message:
+
+        for i in range(0, len(playfair_key)):
+
+            if (j[0] in playfair_key[i] and j[1] in playfair_key[i]):
+                p.append(
+                    playfair_key[i][(playfair_key[i].index(j[0])-1)%5]
+                    +
+                    playfair_key[i][(playfair_key[i].index(j[1])-1)%5]
+                    )
+
+
+            elif(
+                j[0] in playfair_key_transpose[i]
+                and 
+                j[1] in playfair_key_transpose[i]
+                ):
+                p.append(
+                    playfair_key_transpose[i][(playfair_key_transpose[i].index(j[0])-1)%5]
+                    +
+                    playfair_key_transpose[i][(playfair_key_transpose[i].index(j[1])-1)%5]
+                    )
+            else:
+                if(
+                    j[0] in playfair_key[i]
+                    and
+                    j[1] not in playfair_key[i]
+                    ):
+
+                    for _ in range(0, len(playfair_key_transpose)):
+
+                        if j[0] in playfair_key_transpose[_] and j[1] not in playfair_key_transpose[_]:
+
+                            for k in range(0, len(playfair_key)):
+                                if(
+                                    j[1] in playfair_key[k]
+                                    ):
+
+                                    row_number_1 = k
+                                    column_number_1 = playfair_key[i].index(j[0])
+
+                                    row_number_0 = i
+                                    column_number_0 = playfair_key[k].index(j[1])
+
+                                    p.append(
+                                        playfair_key[row_number_0][column_number_0]
+                                        +
+                                        playfair_key[row_number_1][column_number_1]
+                                        )
+    return p
+
+if __name__=="__main__":
+    playfair_message = create_playfair_msg()
+    print(playfair_message)
+    playfair_key = create_playfair_key('PLAYFAIR')
+    print(playfair_key)
+    playfair_key_transpose = transpose(playfair_key)
+    print(playfair_key_transpose)
+    enc = encrypt_message()
+    p = decrypt_message(enc)
+    print(enc)
+    print(p)
